a37db470868a7db58b0731a3acb05e03c12da1f7
Viacheslav Zahorodnyi <viacheslav.zahorodnyi@ambisafe.com>
2024-07-28 22:48:14 +0300

    v1.0.5 Fix types import

--- src/modules.d.ts
+++ src/modules.d.ts
@@ -1,15 +1,8 @@
-import type {
-  FastifyTypeProvider,
-  FastifyPluginOptions,
-  RawServerBase,
-  RawServerDefault,
-  FastifyPluginCallback,
-  FastifyPluginAsync,
-} from "fastify";
 import { Schema, z } from "zod";
-
 declare module "fastify" {
-  interface FastifyTypeProviderDefault {
-    output: this["input"] extends Schema ? z.infer<this["input"]> : unknown;
-  }
+    interface FastifyTypeProviderDefault {
+        output: this["input"] extends Schema ? z.infer<this["input"]> : unknown;
+    }
 }
+export {};
+//# sourceMappingURL=modules.d.ts.map
---
+++ src/modules.ts
@@ -0,0 +1,17 @@
+import type {
+  FastifyTypeProvider,
+  FastifyPluginOptions,
+  RawServerBase,
+  RawServerDefault,
+  FastifyPluginCallback,
+  FastifyPluginAsync,
+} from "fastify";
+import { Schema, z } from "zod";
+
+declare module "fastify" {
+  interface FastifyTypeProviderDefault {
+    output: this["input"] extends Schema ? z.infer<this["input"]> : unknown;
+  }
+}
+
+export { }
